55fb59cb251c468a0e768dba1a3f649d00621db6
dodreamfire <ssoo5324@naver.com>
2025-09-06 17:42:46 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,20 @@
-const CACHE_NAME = 'dodreamfire-cache-v2';
+const CACHE_NAME = 'dodreamfire-cache-v3';
 const urlsToCache = ['./index.html','./manifest.json','./dodreamfirelogo.PNG'];
 
 self.addEventListener('install', e=>{
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>{
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('activate', e=>{
   e.waitUntil(
-    caches.keys().then(keys=>{
-      return Promise.all(
-        keys.map(key=>{
-          if(key !== CACHE_NAME) return caches.delete(key);
-        })
-      );
-    })
+    caches.keys().then(keys=>Promise.all(
+      keys.map(key=>{ if(key !== CACHE_NAME) return caches.delete(key); })
+    ))
   );
 });
 
 self.addEventListener('fetch', e=>{
-  e.respondWith(
-    caches.match(e.request).then(response=>{
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
 });
