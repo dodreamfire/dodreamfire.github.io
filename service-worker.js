bad6a3fdefb3d9bbefc88820008b1cf892ce277d
dodreamfire <ssoo5324@naver.com>
2025-09-06 18:19:03 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,20 @@
-const CACHE_NAME = "dodreamfire-cache-v1";
-const urlsToCache = ["/", "/index.html", "/style.css", "/app.js", "/dodreamfirelogo.PNG"];
+const CACHE_NAME = 'dodream-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/dodreamfirelogo.PNG',
+  // 필요한 js, css 파일 경로 추가
+];
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
